ccb4559eb460cae3762a3322a25518779f477f38
Sai Sridhar Akula <sridhar.akula001@gmail.com>
2021-07-14 14:00:35 +0530

    User List fetch

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -1,17 +1,48 @@
 import React from "react";
 import UsersList from "../components/UsersList";
+import useSWR from "swr";
+import axios from "axios";
+import { Heading, Spinner } from "@chakra-ui/react";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "John Doe",
-      image: "https://bankai-ecommerce.s3.amazonaws.com/media/gorilla.jpeg",
-      places: 3,
-    },
-  ];
+    const getData = async (key) => {
+        const response = await axios.get(key);
+        return response;
+    };
 
-  return <UsersList items={USERS} />;
+    const { data, error } = useSWR("http://localhost:9001/api/users", getData);
+
+    if (!error && !data) {
+        return (
+            <div style={{ textAlign: "center" }}>
+                <Spinner
+                    thickness="4px"
+                    speed="0.65s"
+                    emptyColor="gray.200"
+                    color="magenta"
+                    size="xl"
+                    mx="auto"
+                    mb="1.1rem"
+                    display="block"
+                />
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <Heading textAlign="center" color="whitesmoke" mt="20%">
+                Something went Wrong... &nbsp;{error.message}
+            </Heading>
+        );
+    }
+
+    let found = [];
+    if (data) {
+        found = data.data.users;
+    }
+
+    return <UsersList items={found} />;
 };
 
 export default Users;
